0aeaa46dc696adc9d51b9d636a3aeb97534cf491
JBK2116 <jovani.badinga7@gmail.com>
2026-08-27 21:06:34 -0400

    refactor(nvim): disable lsp diagnostics in diffview buffers

--- nvim/lua/jovani/plugins/diffview.lua
+++ nvim/lua/jovani/plugins/diffview.lua
@@ -113,6 +113,9 @@ return {
         vim.opt_local.wrap = false
         vim.opt_local.list = false
         vim.opt_local.colorcolumn = ""
+        for _, client in pairs(vim.lsp.get_clients({ bufnr = bufnr })) do
+          vim.lsp.buf_detach_client(bufnr, client.id)
+        end
       end,
     },
 
