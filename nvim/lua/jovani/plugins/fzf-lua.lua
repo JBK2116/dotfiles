0a1c62f823a7f64907aff45d902c317f94efd6e5
JBK2116 <jovani.badinga7@gmail.com>
2026-08-31 19:11:50 -0400

    refactor(nvim): improve fzf-lua performance

--- nvim/lua/jovani/plugins/fzf-lua.lua
+++ nvim/lua/jovani/plugins/fzf-lua.lua
@@ -6,7 +6,6 @@
 return {
   "ibhagwan/fzf-lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  -- lazy-load on any of these keymaps
   keys = {
     -- File / Buffer Discovery
     { "<leader>ff", "<cmd>FzfLua files<CR>", desc = "Find Files" },
@@ -14,67 +13,72 @@ return {
     { "<leader>,", "<cmd>FzfLua buffers<CR>", desc = "Buffers" },
     -- Search
     { "<leader>fc", "<cmd>FzfLua live_grep<CR>", desc = "Find Content" },
+    { "<leader>fw", "<cmd>FzfLua grep_cword<CR>", desc = "Grep Word Under Cursor" },
+    { "<leader>fR", "<cmd>FzfLua live_grep_resume<CR>", desc = "Resume Last Grep" },
     -- LSP
     { "gd", "<cmd>FzfLua lsp_definitions<CR>", desc = "Goto Definition" },
     { "gD", "<cmd>FzfLua lsp_declarations<CR>", desc = "Goto Declaration" },
-    {
-      "gr",
-      "<cmd>FzfLua lsp_references<CR>",
-      nowait = true,
-      desc = "References",
-    },
+    { "gr", "<cmd>FzfLua lsp_references<CR>", nowait = true, desc = "References" },
     { "gI", "<cmd>FzfLua lsp_implementations<CR>", desc = "Goto Implementation" },
     { "gy", "<cmd>FzfLua lsp_typedefs<CR>", desc = "Goto Type Definition" },
     { "<leader>fs", "<cmd>FzfLua lsp_document_symbols<CR>", desc = "Document Symbols" },
-    {
-      "<leader>fS",
-      "<cmd>FzfLua lsp_live_workspace_symbols<CR>",
-      desc = "Workspace Symbols",
-    },
+    { "<leader>fS", "<cmd>FzfLua lsp_live_workspace_symbols<CR>", desc = "Workspace Symbols" },
     -- Commands
     { "<leader>fa", "<cmd>FzfLua commands<CR>", desc = "Commands" },
     -- Colorschemes
     { "<leader>uC", "<cmd>FzfLua colorschemes<CR>", desc = "Colorschemes" },
   },
-  opts = {
-    -- Default fzf behavior
-    fzf_opts = {
-      ["--layout"] = "reverse",
-      ["--info"] = "inline",
-    },
-    winopts = {
-      width = 0.85,
-      height = 0.85,
-      row = 0.35,
-      col = 0.5,
-      preview = {
-        default = "bat",
-        border = "rounded",
+  opts = function()
+    local actions = require("fzf-lua").actions
+    return {
+      -- Default fzf behavior
+      fzf_opts = {
+        ["--layout"] = "reverse",
+        ["--info"] = "inline",
+        ["--history"] = vim.fn.stdpath("data") .. "/fzf-lua-history",
       },
-    },
-    keymap = {
-      builtin = {
-        ["<C-c>"] = "abort",
+      winopts = {
+        width = 0.85,
+        height = 0.85,
+        row = 0.35,
+        col = 0.5,
+        preview = {
+          default = "bat",
+          border = "rounded",
+          layout = "flex",
+          flip_columns = 120,
+        },
       },
-      fzf = {
-        ["ctrl-c"] = "abort",
+      keymap = {
+        builtin = {
+          ["<C-c>"] = "abort",
+        },
+        fzf = {
+          ["ctrl-c"] = "abort",
+        },
       },
-    },
-    -- File finder: hidden files, follow symlinks, respect .gitignore
-    files = {
-      fd_opts = [[--color=never --type f --hidden --follow --exclude .git]],
-    },
-    -- Live grep: hidden files, follow symlinks
-    grep = {
-      rg_opts = [[--color=never --hidden --follow --no-heading --column --line-number]],
-    },
-    -- Smart-case matching (mirrors vim.o.smartcase / vim.o.ignorecase)
-    defaults = {
-      git_icons = false,
-      file_icons = true,
-      color_icons = true,
-    },
-  },
+      -- File finder: hidden files, follow symlinks, respect .gitignore, skip noise dirs
+      files = {
+        fd_opts = [[--color=never --type f --hidden --follow --exclude .git --exclude node_modules --exclude .cache]],
+        actions = {
+          ["ctrl-q"] = actions.file_edit_or_qf,
+        },
+      },
+      -- Live grep: hidden files, follow symlinks, exclude .git
+      grep = {
+        rg_opts = [[--color=never --hidden --follow --no-heading --column --line-number --glob '!.git/*' --glob '!node_modules/*']],
+        actions = {
+          ["ctrl-q"] = actions.file_edit_or_qf,
+        },
+      },
+      -- Smart-case matching (mirrors vim.o.smartcase / vim.o.ignorecase)
+      defaults = {
+        git_icons = false,
+        file_icons = true,
+        color_icons = true,
+      },
+    }
+  end,
   config = function(_, opts)
     require("fzf-lua").setup(opts)
   end,
